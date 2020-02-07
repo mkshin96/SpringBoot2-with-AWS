90299ea6da22a2f45691f8d33372b7fb2b280a28
mkshin96 <tlsanrhs96@gmail.com>
2020-02-07 18:47:26 +0900

    ~ chapter5
    - Applied OAuth2 with Spring Security
    - Applied Session Repository to MySQL
    - Create @LoginUser using HandlerMethodArgumentResolver

--- freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,7 +1,10 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import com.samskivert.mustache.DefaultCollector;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +13,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
-public class IndexController {
+public class IndexController extends DefaultCollector {
 
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("name", user.getName());
+        }
         return "index";
     }
 
